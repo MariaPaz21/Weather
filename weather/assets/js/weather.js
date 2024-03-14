6dc0e36e8ab62a3a0090db98bd94d894c12fe2ef
MariaPaz21 <145079814+MariaPaz21@users.noreply.github.com>
2024-03-14 18:36:27 -0500

    Update weather.js

--- weather/assets/js/weather.js
+++ weather/assets/js/weather.js
@@ -1,109 +1,109 @@
 import { weather_data } from './data.js';
+
 function CiudadTraer(ciudad) { 
-    let ciudadinfo
+    let ciudadinfo;
     weather_data.forEach(function (objeto) {
-        if (objeto.city === ciudad) { ciudadinfo = objeto }
-    })
+        if (objeto.city === ciudad) { ciudadinfo = objeto; }
+    });
     return ciudadinfo;
 }
+
 let loadDayForecastData = (ciudad) => {
-    let [guayaquil, ambato, tena] = weather_data
-    let { city, date, maxtemperature, mintemperature, cloudiness, wind, rainfall, forecast_today, ...other} = guayaquil
-    let cityHTML = document.getElementById('city')
-    cityHTML.innerHTML = city;
+    let infoCity = CiudadTraer(ciudad);
 
-    let datehtml = document.getElementById('date')
-    datehtml.innerHTML = date
+    let cityHTML = document.getElementById('city');
+    cityHTML.innerHTML = infoCity.city;
 
-    let tempmaxhtml = document.getElementById('maxtemperature')
-    tempmaxhtml.innerHTML = maxtemperature
+    let datehtml = document.getElementById('date');
+    datehtml.innerHTML = infoCity.date;
 
-    let tempminhtml = document.getElementById('mintemperature')
-    tempminhtml.innerHTML = mintemperature
+    let tempmaxhtml = document.getElementById('maxtemperature');
+    tempmaxhtml.innerHTML = infoCity.maxtemperature;
 
-    let cloudhtml = document.getElementById('cloudiness')
-    cloudhtml.innerHTML = cloudiness
+    let tempminhtml = document.getElementById('mintemperature');
+    tempminhtml.innerHTML = infoCity.mintemperature;
 
-    let windhtml = document.getElementById('wind')
-    windhtml.innerHTML= wind
+    let cloudhtml = document.getElementById('cloudiness');
+    cloudhtml.innerHTML = infoCity.cloudiness;
 
-    let rainhtml = document.getElementById('rainfall')
-    rainhtml.innerHTML = rainfall
+    let windhtml = document.getElementById('wind');
+    windhtml.innerHTML= infoCity.wind;
 
-    let [late, night] = guayaquil.forecast_today
+    let rainhtml = document.getElementById('rainfall');
+    rainhtml.innerHTML = infoCity.rainfall;
 
-    let late_iconhtml = document.getElementById('late_icon')
-    late_iconhtml.innerHTML = late.icon
+    let [late, night] = infoCity.forecast_today;
+
+    let late_iconhtml = document.getElementById('late_icon');
+    late_iconhtml.innerHTML = late.icon;
     
-    let late_temphtml = document.getElementById('late_temperature')
-    late_temphtml.innerHTML = late.temperature
+    let late_temphtml = document.getElementById('late_temperature');
+    late_temphtml.innerHTML = late.temperature;
 
-    let late_forecasthtml = document.getElementById('late_forecast')
-    late_forecasthtml.innerHTML = late.forecast
+    let late_forecasthtml = document.getElementById('late_forecast');
+    late_forecasthtml.innerHTML = late.forecast;
 
-    let late_txthtml = document.getElementById('late_text')
-    late_txthtml.innerHTML = late.text
+    let late_txthtml = document.getElementById('late_text');
+    late_txthtml.innerHTML = late.text;
 
 
-    let night_iconhtml = document.getElementById('night_icon')
-    night_iconhtml.innerHTML = night.icon
+    let night_iconhtml = document.getElementById('night_icon');
+    night_iconhtml.innerHTML = night.icon;
 
-    let night_temphtml = document.getElementById('night_temperature')
-    night_temphtml.innerHTML = night.temperature
+    let night_temphtml = document.getElementById('night_temperature');
+    night_temphtml.innerHTML = night.temperature;
 
-    let night_forecasthtml = document.getElementById('night_forecast')
-    night_forecasthtml.innerHTML = night.forecast
+    let night_forecasthtml = document.getElementById('night_forecast');
+    night_forecasthtml.innerHTML = night.forecast;
 
-    let night_txthtml = document.getElementById('night_text')
-    night_txthtml.innerHTML = night.text
-
+    let night_txthtml = document.getElementById('night_text');
+    night_txthtml.innerHTML = night.text;
 }
 
-let loadWeekForecastData = () => {
-    let week = document.getElementsByClassName("list-group")
-    let infoCity = CiudadTraer(ciudad)
+let loadWeekForecastData = (ciudad) => {
+    let week = document.getElementsByClassName("list-group")[0];
+    let infoCity = CiudadTraer(ciudad);
 
-    week[0].innerHTML = ""
+    week.innerHTML = "";
+
     for (let day of infoCity.forecast_week) {
-        week[0].innerHTML = week[0].innerHTML +
-            `<li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+        week.innerHTML += `
+            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
                 <div class="d-flex flex-column">
                     <h6 class="mb-1 text-dark font-weight-bold text-sm">${day.text}</h6>
                     <span class="text-xs">${day.date}</span>
                 </div>
                 <div class="d-flex align-items-center ">
-                    <span class="font-weight-bold te|xt-dark mx-2">${day.temperature.max}</span> |  <span class="text-dark mx-2">${dia.temperature.min}</span>
+                    <span class="font-weight-bold text-dark mx-2">${day.temperature.max}</span> |  <span class="text-dark mx-2">${day.temperature.min}</span>
                     <div class="ms-4"><i class="material-icons fs-2 me-1 rainy">${day.icon}</i></div>
                 </div>
-            </li>`
+            </li>`;
     }
-
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    let cities = document.getElementById("dropdownMenuButton")
-    cities.addEventListener
+document.addEventListener("DOMContentLoaded", () => {
+    let cities = document.getElementById("dropdownMenuButton");
+
     while (cities.firstChild) {
         cities.removeChild(cities.firstChild);
     }
-    for (let i = 0; i < weather_data.length; i++) {
-        let choice = new Option(weather_data[i].city, weather_data[i].city)
-        cities.add(choice)
-    }
 
-    loadDayForecastData(cities.valor);
+    for (let i = 0; i < weather_data.length; i++) {
+        let choice = new Option(weather_data[i].city, weather_data[i].city);
+        cities.add(choice);
+    }
 
     let cargarbtn = document.getElementById("loadinfo");
 
-    cargarbtn.addEventListener("click", function () {
-        loadWeekForecastData(cities.value);
+    cargarbtn.addEventListener("click", () => {
+        let ciudad = cities.value;
+        loadWeekForecastData(ciudad);
+        loadDayForecastData(ciudad);
     });
 
-
-    cities.addEventListener("change", function () {
+    cities.addEventListener("change", () => {
         let ciudad = cities.value;
-        loadWeekForecastData(ciudad.value);
-        loadDayForecastData(ciudad.value);
-    })
-
+        loadWeekForecastData(ciudad);
+        loadDayForecastData(ciudad);
+    });
 });
